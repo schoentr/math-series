010b191ac9eaeb2919aa869a1e77b0823f2692e9
Tim Schoen <schoentr@gmail.com>
2019-02-19 15:20:28 -0800

    sum tests

--- series.py
+++ series.py
@@ -1,4 +1,4 @@
-def fibSeq(value):
+def fibonacci(value):
     fib_list = [0, 1]
     if value > 2:
         for i in range(2, value):
@@ -10,4 +10,14 @@ def lucas(value):
     if value > 2:
         for i in range(2, value):
             lucas_list.append(lucas_list[i - 2] + lucas_list[i - 1])
-    return lucas_list[value-1]
+    return lucas_list[value - 1]
+def sum_series(value, start=0, second=1):
+    sum_list = []
+    sum_list.append(start)
+    sum_list.append(second)
+    if value > 2:
+        for i in range(2, value):
+            sum_list.append(sum_list[i - 2] + sum_list[i - 1])
+    return sum_list[value - 1]
+
+  
--- test_series.py
+++ test_series.py
@@ -1,22 +1,24 @@
-from series import  fibSeq, lucas
-
+from series import fibonacci, lucas, sum_series
+# ********* fibonacci ********
 def test_third_index():
-    actual = fibSeq(3)
+    actual = fibonacci(3)
     expected = 1
     assert expected == actual
 
 def test_fourth_index():
-    actual = fibSeq(4)
+    actual = fibonacci(4)
     expected = 2
     assert expected == actual
 def test_fifth_index():
-    actual = fibSeq(5)
+    actual = fibonacci(5)
     expected = 3
     assert expected == actual
 def test_sixth_index():
-    actual = fibSeq(6)
+    actual = fibonacci(6)
     expected = 5
     assert expected == actual
+
+#  ******  lucas testing   ********
 def test_lucas_third_index():
     actual = lucas(3)
     expected = 3
@@ -28,4 +30,18 @@ def test_lucas_forth_index():
 def test_lucas_fifth_index():
     actual = lucas(5)
     expected = 7
+    assert expected == actual
+
+# *******  sum testing ***********
+def test_lucas_fifth_index():
+    actual = sum_series(5,0,1)
+    expected = 3
+    assert expected == actual
+def test_lucas_fifth_index():
+    actual = sum_series(5,2,1)
+    expected = 7
+    assert expected == actual
+def test_lucas_fifth_index():
+    actual = sum_series(6,2,1)
+    expected = 11
     assert expected == actual
